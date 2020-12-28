a2c45d1c8689f1d75c1f7cc7877cde62e7efd373
eunjeong09 <ejko0911@gmail.com>
2020-12-28 22:45:40 +0900

    add modal

--- js/index.js
+++ js/index.js
@@ -1,18 +1,44 @@
 //javascript
-console.log("dd");
+console.log("check");
 
 //login
 
 //add skill
 document.getElementById("addSkill").onclick = function () {
   console.log("에드스킬");
-  var modal = document.getElementsByClassName('addModal');
-  removeClass(modal,'hide');
-  addClass(modal,'show');
+  // var modal = document.getElementsByClassName('addModal');
+
+  const modalWrap = document.getElementById("addModalWrap");
+  removeClass(modalWrap, "hide");
+  addClass(modalWrap, "show");
+
+  document.getElementsByClassName("modalTitle")[0].innerHTML="Add Skill";
+};
+
+//파일 업로드
+document.getElementById("upload").onclick = function () {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("POST", "./php/upload.php", true);
+  xhr.setRequestHeader("Content-type", "image");
+
+  var file = document.getElementById("addFile").files[0];
+  if (file) {
+    var formdata = new FormData();
+    formdata.append("addFile", file);
+    xhr.send(formdata);
+  }
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      console.log("file업로드 성공?");
+      //some code
+    }
+  };
 };
 
 //add work
 
+//모달 재활용,,,
 
 // addClass
 function addClass(element, className) {
